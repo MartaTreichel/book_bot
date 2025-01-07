b96c05b49134d98fdaedd8953a649113586158eb
MartaTreichel <marta.kazmierczak@zohomail.eu>
2025-01-07 10:34:52 +0100

    add words_counter

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 path_to_file = "books/frankenstein.txt"
 
+def words_counter(text):
+    words = text.split()
+    number_of_words = 0
+    for _ in words:
+        number_of_words += 1
+    return number_of_words
+
 def main():
     with open(path_to_file) as file:
         file_content = file.read()
-    print(file_content)
+        number_of_words_in_book = words_counter(file_content)
+    print(number_of_words_in_book)
 
 main()
