f985281c18dccf6771263ecb82ad8b002517766f
MartaTreichel <marta.kazmierczak@zohomail.eu>
2025-01-09 10:18:08 +0100

    add letter_counter

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 path_to_file = "books/frankenstein.txt"
 
 def words_counter(text):
@@ -8,40 +10,14 @@ def words_counter(text):
     return number_of_words
 
 def letter_counter(text):
-    letters = {}
-    text = text.lower()
-    for letter in text:
-        if letter.isalpha() == True:
-            letters[letter] = letters.get(letter, 0) +1
-    # for letter, ilosc in letters.items():
-    #     print(f"{letter} - {ilosc}")
-    return letters
-
-# def letter_counter(text):
-#     letters = {}
-#     letter = 0
-#     for a in text:
-#         text = text.lower()
-#         if a in letters:
-#             letters[a] += 1
-#         else:
-#             letters[a] = 1
-#     return letters
-
-#1. przeiterowac przez wszystkie litery w calym tekscie i policzyc ich wystapienia
-#2. podczas petli uzyc lower
-#3. sprawdz czy dany znak jest litera - .isalpha
-#4. uzyj w main
+    return Counter(letter for letter in text.lower() if letter.isalpha())
 
 def main():
     with open(path_to_file) as file:
         file_content = file.read()
         number_of_words_in_book = words_counter(file_content)
-        letters = letter_counter(file_content)
+        letter = letter_counter(file_content)
     print(number_of_words_in_book)
-    print(letters)
+    print(letter)
 
-
-
-# {'p': 6121, 'r': 20818, 'o': 25225, ...
 main()
