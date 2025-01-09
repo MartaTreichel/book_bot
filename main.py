c04e3f4592dcfb7771e437098edbb02d00526b95
MartaTreichel <marta.kazmierczak@zohomail.eu>
2025-01-09 10:32:12 +0100

    add report

--- main.py
+++ main.py
@@ -16,8 +16,13 @@ def main():
     with open(path_to_file) as file:
         file_content = file.read()
         number_of_words_in_book = words_counter(file_content)
-        letter = letter_counter(file_content)
-    print(number_of_words_in_book)
-    print(letter)
+        number_of_letters_in_book = letter_counter(file_content)
+        print(f"--Begin report of {path_to_file} ---")
+        print(f"{number_of_words_in_book} words found in the document")
+        print()
+        for letter, count in number_of_letters_in_book.items():
+            print(f"The '{letter}' character was found {count} times")
+        print("--- End report ---")
+
 
 main()
